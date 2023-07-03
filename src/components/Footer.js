fd288cfa74bb72abf5dca25c164821b85d9ee8c1
rhazem13 <rhazem13@yahoo.com>
2023-07-03 05:43:48 +0300

    link social buttons to yours

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,14 +16,20 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/rhazem13/">
+                <img src={navIcon1} alt="Icon" />
+              </a>
+              <a href="https://www.facebook.com/HazemRg/">
+                <img src={navIcon2} alt="Icon" />
+              </a>
+              <a href="https://www.instagram.com/hazemragabb/">
+                <img src={navIcon3} alt="Icon" />
+              </a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright 2023. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
     </footer>
-  )
+  );
 }
