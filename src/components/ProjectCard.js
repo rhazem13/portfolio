99f771d29e1389d6ef601ff4bbc6c6398db463bd
rhazem13 <rhazem13@yahoo.com>
2023-07-07 06:06:27 +0300

    link projects to live preview

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,34 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, projectUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} />
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
+      {projectUrl !== "" ? (
+        <a
+          href={projectUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ textDecoration: "none" }}
+        >
+          <div className="proj-imgbx">
+            <img src={imgUrl} alt="Project" />
+            <div className="proj-txtx">
+              <h4 style={{ color: "white", textDecoration: "none" }}>
+                {title}
+              </h4>
+              <span style={{ color: "white" }}>{description}</span>
+            </div>
+          </div>
+        </a>
+      ) : (
+        <div className="proj-imgbx">
+          <img src={imgUrl} alt="Project" />
+          <div className="proj-txtx">
+            <h4 style={{ color: "white", textDecoration: "none" }}>{title}</h4>
+            <span style={{ color: "white" }}>{description}</span>
+          </div>
         </div>
-      </div>
+      )}
     </Col>
-  )
-}
+  );
+};
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,19 +12,22 @@ export const Projects = () => {
   const projects = [
     {
       title: "Quantum",
-      description: "Design & Development",
+      description: "Live Preview",
       imgUrl: projImg1,
+      projectUrl: "http://www.quantumme.net/",
     },
     {
       title: "BeFriends",
-      description: "Design & Development",
+      description: "Live Preview",
       imgUrl: projImg2,
+      projectUrl: "https://befriends-it7g.onrender.com/#/feed",
     },
     {
       title: "Escanor",
-      description: "Design & Development",
+      description: "Not live currently",
       imgUrl: projImg3,
-    }
+      projectUrl: "",
+    },
   ];
 
   return (
@@ -33,13 +36,22 @@ export const Projects = () => {
         <Row>
           <Col size={12}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Here are some of the projects i made so far.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    {/* <Nav.Item>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Projects</h2>
+                  <p>Here are some of the projects i made so far.</p>
+                  <p style={{fontSize:'16px'}}>Click on the project to open live preview</p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      {/* <Nav.Item>
                       <Nav.Link eventKey="first">Tab 1</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
@@ -48,36 +60,47 @@ export const Projects = () => {
                     <Nav.Item>
                       <Nav.Link eventKey="third">Tab 3</Nav.Link>
                     </Nav.Item> */}
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+                    </Nav>
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="section">
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Cumque quam, quod neque provident velit, rem
+                          explicabo excepturi id illo molestiae blanditiis,
+                          eligendi dicta officiis asperiores delectus quasi
+                          inventore debitis quo.
+                        </p>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Cumque quam, quod neque provident velit, rem
+                          explicabo excepturi id illo molestiae blanditiis,
+                          eligendi dicta officiis asperiores delectus quasi
+                          inventore debitis quo.
+                        </p>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
       <img className="background-image-right" src={colorSharp2}></img>
     </section>
-  )
+  );
 }
